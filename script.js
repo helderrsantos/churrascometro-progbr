9a0d2ad33c1051b4d087358fe0d75cc71e9f3a45
Helder Rodrigo <helderr_santosd@hotmail.com>
2022-06-08 12:23:25 -0300

    ok

--- script.js
+++ script.js
@@ -9,8 +9,7 @@ let inputAdultos = document.getElementById("adultos");
 let inputCriancas = document.getElementById("criancas");
 let inputDuracao = document.getElementById("duracao");
 
-let resultado = document.getElementById("resultado")
-
+let resultado = document.getElementById("resultado");
 
 function calcular(){
     console.log("Calculando...");
